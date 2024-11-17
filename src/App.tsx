8d7a4ddd9ea5c7ffe1698dd14ca6ea60af32351e
Araryarch <araryaarch@gmail.com>
2024-11-17 10:31:09 +0700

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import homa from '/images/anomalihoma.png'
 
 const App = () => {
   return (
-    <div className='container'>
-      <img
-        src={homa}
-        alt='homa'
-      />
+    <div className='w-full min-h-screen flex justify-center items-center bg-black'>
+      <div className='grid xl:grid-cols-3 md:grid-cols-2 gap-6 p-4 grid-cols-1'>
+        {[...Array(6)].map((_, index) => (
+          <div
+            key={index}
+            className='flex justify-center'
+          >
+            <img
+              src={homa}
+              alt={`Homa Image ${index + 1}`}
+              className='object-cover w-96 h-96 rounded-lg shadow-lg'
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
